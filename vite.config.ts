4437a9d12b3374e101232c62f1f470e0215cddc9
Neo Mohsenvand <mmv@mit.edu>
2025-02-22 02:08:30 -0600

    fix deploy gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/dpp/',
+  plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js',
+        assetFileNames: (assetInfo) => {
+          return `assets/${assetInfo.name}`;
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
+  },
+  server: {
+    port: 3000,
+    host: true,
+    open: true
   }
 })
